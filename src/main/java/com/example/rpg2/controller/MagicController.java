c8f221c8a6a16b022b21efca7b2c03078e51ba8c
tetsu <root2120@gmail.com>
2026-05-23 21:36:47 +0900

    リファクタリング

--- src/main/java/com/example/rpg2/controller/MagicController.java
+++ src/main/java/com/example/rpg2/controller/MagicController.java
@@ -20,271 +20,200 @@ import lombok.RequiredArgsConstructor;
 public class MagicController {
 	
 	private final MagicRepository magicRepository;
-	private final HttpSession	  session;
-	
-	//行動する側の情報を管理
-	private Integer myKeys;
-	private Magic   magic;
-	
 
 	//すべての魔法の選択画面を表示
-	@GetMapping( "/magic/{key}" )
-	public ModelAndView magic( @PathVariable( name = "key" ) int key ,
-								ModelAndView mv ) {
-		
+	@GetMapping( "/magic/{myKey}" )
+	public ModelAndView magic( @PathVariable int myKey ,
+								ModelAndView mv , HttpSession session) {
 		mv.setViewName( "battle" );
 		Battle battle = (Battle)session.getAttribute( "battle" );
-		
-		myKeys = key;
-		
 		//発動可能な魔法一覧を表示
-		List<Magic> magicList = battle.getPartyMap().get( key ).getMagicList();
+		List<Magic> magicList = battle.getPartyMap().get( myKey ).getMagicList();
 		mv.addObject( "magicList" , magicList );
-		mv.addObject( "key" , myKeys );
+		mv.addObject( "mykey" , myKey );
 		session.setAttribute( "mode" , "magic" );
-		
-		System.out.println( magicList.get(0).getName() );
-		
 		return mv;
-		
 	}
-	
-	
+
 	//攻撃魔法の選択画面を表示
-	@GetMapping( "/magic/attack/{key}" )
-	public ModelAndView magicA( @PathVariable( name = "key" ) int key ,
-								ModelAndView mv ) {
-		
+	@GetMapping( "/magic/attack/{myKey}" )
+	public ModelAndView magicA( @PathVariable int myKey ,
+								ModelAndView mv , HttpSession session) {
 		mv.setViewName( "battle" );
 		Battle battle = (Battle)session.getAttribute( "battle" );
-		
-		myKeys = key;
-		
 		//発動可能な魔法一覧を表示
-		List<Magic> magicList = battle.getPartyMap().get( key ).getMagicList();
+		List<Magic> magicList = battle.getPartyMap().get( myKey ).getMagicList();
 		List<Magic> magicListA = magicList.stream()
 				.filter( s -> s.getCategory().equals( "targetenemy" ))
 				.filter( s -> s.getBuffcategory().equals( "no" ) )
 				.collect( Collectors.toList() );
-
 		mv.addObject( "magicList" , magicListA );
-		mv.addObject( "key" , myKeys );
+		mv.addObject( "mykey" , myKey );
 		session.setAttribute( "mode" , "magic" );
-		
 		return mv;
-		
 	}
 	
 	//回復魔法の選択画面を表示
-	@GetMapping( "/magic/recovery/{key}" )
-	public ModelAndView magicR( @PathVariable( name = "key" ) int key ,
-								ModelAndView mv ) {
-		
+	@GetMapping( "/magic/recovery/{myKey}" )
+	public ModelAndView magicR( @PathVariable int myKey ,
+								ModelAndView mv , HttpSession session) {
 		mv.setViewName( "battle" );
 		Battle battle = (Battle)session.getAttribute( "battle" );
-		
-		myKeys = key;
-		
 		//発動可能な魔法一覧を表示
-		List<Magic> magicList = battle.getPartyMap().get( key ).getMagicList();
+		List<Magic> magicList = battle.getPartyMap().get( myKey ).getMagicList();
 		List<Magic> magicListR = magicList.stream()
 				.filter( s -> s.getCategory().equals( "targetally" ) || s.getCategory().equals( "resuscitationmagic" ) )
 				.filter( s -> s.getBuffcategory().equals( "no" ) )
 				.collect( Collectors.toList() );
-
 		mv.addObject( "magicList" , magicListR );
-		mv.addObject( "key" , myKeys );
+		mv.addObject( "mykey" , myKey );
 		session.setAttribute( "mode" , "magic" );
-		
 		return mv;
-		
 	}
 	
 	//補助魔法の選択画面を表示
-	@GetMapping( "/magic/buff/{key}" )
-	public ModelAndView magicB( @PathVariable( name = "key" ) int key ,
-								ModelAndView mv ) {
-		
+	@GetMapping( "/magic/buff/{myKey}" )
+	public ModelAndView magicB( @PathVariable int myKey ,
+								ModelAndView mv , HttpSession session) {
 		mv.setViewName( "battle" );
 		Battle battle = (Battle)session.getAttribute( "battle" );
-		
-		myKeys = key;
-		
 		//発動可能な魔法一覧を表示
-		List<Magic> magicList = battle.getPartyMap().get( key ).getMagicList();
+		List<Magic> magicList = battle.getPartyMap().get( myKey ).getMagicList();
 		List<Magic> magicListB = magicList.stream()
 				.filter( s -> s.getCategory().equals( "targetally" ) )
 				.filter( s -> !s.getBuffcategory().equals( "no" ) )
 				.collect( Collectors.toList() );
-
 		mv.addObject( "magicList" , magicListB );
-		mv.addObject( "key" , myKeys );
+		mv.addObject( "mykey" , myKey );
 		session.setAttribute( "mode" , "magic" );
-		
 		return mv;
-		
 	}
-	
-	
+
 	//妨害魔法の選択画面を表示
-	@GetMapping( "/magic/debuff/{key}" )
-	public ModelAndView magicD( @PathVariable( name = "key" ) int key ,
-								ModelAndView mv ) {
-		
+	@GetMapping( "/magic/debuff/{myKey}" )
+	public ModelAndView magicD( @PathVariable int myKey ,
+								ModelAndView mv , HttpSession session) {
 		mv.setViewName( "battle" );
 		Battle battle = (Battle)session.getAttribute( "battle" );
-		
-		myKeys = key;
-		
 		//発動可能な魔法一覧を表示
-		List<Magic> magicList = battle.getPartyMap().get( key ).getMagicList();
+		List<Magic> magicList = battle.getPartyMap().get( myKey ).getMagicList();
 		List<Magic> magicListA = magicList.stream()
 				.filter( s -> s.getCategory().equals( "targetenemy" ))
 				.filter( s -> !s.getBuffcategory().equals( "no" ) )
 				.collect( Collectors.toList() );
-
 		mv.addObject( "magicList" , magicListA );
-		mv.addObject( "key" , myKeys );
+		mv.addObject( "mykey" , myKey );
 		session.setAttribute( "mode" , "magic" );
-		
 		return mv;
-		
 	}
-	
-	
+
 	//魔法を選択
-	@GetMapping( "/magic/add/{id}" )
-	public ModelAndView magic2( @PathVariable( name = "id" ) int id , 
-								ModelAndView mv ) {
-		
+	@GetMapping( "/magic/add/{id}/{myKey}" )
+	public ModelAndView magic2( @PathVariable( name = "id" ) int id ,
+								@PathVariable int myKey ,
+								ModelAndView mv , HttpSession session) {
 		mv.setViewName( "battle" );
 		Battle battle = (Battle)session.getAttribute( "battle" );
-		magic = magicRepository.findById( id ).get();
-		
-		
+		Magic magic = magicRepository.findById( id ).get();
+
 		//単体が対象の魔法を選択
 		if( magic.getRange().equals( "single" ) ) {
-			
 			switch( magic.getCategory() ) {
-			
 				//味方が対象の魔法
 				case "targetally":
 					session.setAttribute( "mode" , "targetAllyMagic" );
 					break;
-				
 				//敵が対象の魔法
 				case "targetenemy":
 					session.setAttribute( "mode" , "targetMonsterMagic" );
 					break;
-				
 				//蘇生魔法
 				case "resuscitationmagic":
 					session.setAttribute( "mode" , "targetDeathAllyMagic" );
 					break;
 			}
-			
 			//選択した内容をオブジェクトに保存
 			session.setAttribute( "battle" , battle );
-			
+			session.setAttribute( "magic" , magic );
 			return mv;
-			
 		}
-		
-		
+
 		//グループが対象の魔法（敵のみが対象
 		if( magic.getRange().equals( "group" ) ) {
-			
 			session.setAttribute( "mode" , "targetGroupNameMonsterMagic" );
-			
 			//選択した内容をオブジェクトに保存
 			session.setAttribute( "battle" , battle );
-			
+			session.setAttribute( "magic" , magic );
 			return mv;
 		}
-		
-		
+
 		//対象が全体の魔法
 		if( magic.getRange().equals( "whole" )) {
-			
 			switch( magic.getCategory() ) {
-			
 				//味方全体が対象の魔法
 				case "targetally":
-					battle.selectionAllyMagic( myKeys , magic );
+					battle.selectionAllyMagic( myKey , magic );
 					session.setAttribute( "mode" , "log" );
-					
 					break;
-				
 				//敵全体の魔法
 				case "targetenemy":
-					battle.selectionMonsterMagic( myKeys , magic );
+					battle.selectionMonsterMagic( myKey , magic );
 					session.setAttribute( "mode" , "log" );	
-					
 					break;
-					
 				//全体蘇生魔法
 				case "resuscitationmagic":
-					battle.selectionAllyMagic( myKeys , magic );
+					battle.selectionAllyMagic( myKey , magic );
 					session.setAttribute( "mode" , "log" );
 					break;
 			}
-					
 			//選択した内容をオブジェクトに保存
 			session.setAttribute( "battle" , battle );
-
+			session.setAttribute( "magic" , magic );
 			return mv;
-			
 		}
-
 		return mv;
 	}
-	
-	
+
 	//ターゲット選択(味方への魔法）
-	@GetMapping( "/target/magic/ally/{key}" )
-	public ModelAndView targetAlly( @PathVariable( name = "key" ) int key ,
-									ModelAndView mv ) {
+	@GetMapping( "/target/magic/ally/{myKey}/{targetKey}" )
+	public ModelAndView targetAlly( @PathVariable int myKey ,
+									@PathVariable int targetKey ,
+									ModelAndView mv , HttpSession session) {
 		
 		mv.setViewName( "battle" );
 		Battle battle = (Battle)session.getAttribute( "battle" );
-		battle.selectionAllyMagic( myKeys , key , magic );
-		
+		Magic magic = (Magic)session.getAttribute( "magic" );
+		battle.selectionAllyMagic( myKey , targetKey , magic );
 		session.setAttribute( "battle" , battle );
 		session.setAttribute( "mode" , "log" );
-		
 		return mv;
 	}
-	
-	
+
 	//ターゲット選択(攻撃魔法)
-	@GetMapping( "/target/magic/monster/{key}" )
-	public ModelAndView magicTargetMonster( @PathVariable( name = "key" ) int key ,
-											ModelAndView mv ) {
-
+	@GetMapping( "/target/magic/monster/{myKey}/{targetKey}" )
+	public ModelAndView magicTargetMonster( @PathVariable int myKey ,
+									        @PathVariable int targetKey ,
+											ModelAndView mv , HttpSession session) {
 		mv.setViewName( "battle" );
 		Battle battle = (Battle)session.getAttribute( "battle" );
-		battle.selectionMonsterMagic( myKeys , key , magic );
-		
+		Magic magic = (Magic)session.getAttribute( "magic" );
+		battle.selectionMonsterMagic( myKey , targetKey , magic );
 		session.setAttribute( "battle" , battle );
 		session.setAttribute( "mode" , "log" );
-		
 		return mv;
 	}
-	
-	
+
 	//ターゲット選択(グループ攻撃魔法)
-	@GetMapping( "/target/magic/monsterGroup/{name}" )
+	@GetMapping( "/target/magic/monsterGroup/{name}/{myKey}" )
 	public ModelAndView magicTargetMonsterGroup( @PathVariable( name = "name" ) String name ,
-												 ModelAndView mv ) {
-
+												 @PathVariable int myKey ,
+												 ModelAndView mv , HttpSession session) {
 		mv.setViewName( "battle" );
 		Battle battle = (Battle)session.getAttribute( "battle" );
-		battle.selectionMonsterMagic( name , myKeys ,  magic );
-		
+		Magic magic = (Magic)session.getAttribute( "magic" );
+		battle.selectionMonsterMagic( name , myKey , magic );
 		session.setAttribute( "battle" , battle );
 		session.setAttribute( "mode" , "log" );
-		
 		return mv;
 	}
-
 }
